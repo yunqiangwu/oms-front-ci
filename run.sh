14ef464b1f458fab4b1f36d2236efa68187f203d
yunqiangwu <yunqiang.wu@hand-china.com>
2018-03-20 06:46:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,17 @@ if [ x${BRANCH} == x ]; then
   export BRANCH=develop
 fi
 
-if [ x${OMS_HOST} == x ]; then
-  export OMS_HOST=ci.jajabjbj.top
-fi
+# if [ x${OMS_HOST} == x ]; then
+#   export OMS_HOST=ci.jajabjbj.top
+# fi
 
-if [ x${NOTICE_URL} == x ]; then
-  export NOTICE_URL=http://10.1.255.119:8007/notice
-fi
+# if [ x${NOTICE_URL} == x ]; then
+#   export NOTICE_URL=http://10.1.255.119:8007/notice
+# fi
 
-if [ x${PROXY_HOOK_URL} == x ]; then
-  export PROXY_HOOK_URL=ws://git-webhook-proxy-server-front-server.193b.starter-ca-central-1.openshiftapps.com
-fi
+# if [ x${PROXY_HOOK_URL} == x ]; then
+#   export PROXY_HOOK_URL=ws://git-webhook-proxy-server-front-server.193b.starter-ca-central-1.openshiftapps.com
+# fi
 
 git clone -b ${BRANCH} https://rdc.hand-china.com/gitlab/hand-ln/oms-front.git
 cd oms-front
@@ -23,8 +23,7 @@ cd oms-front
 cnpm i
 pwd
 
-cnpm i -g webhook-client
 npm run build:dll
-webhook-c --start-cmd "npm start" --stop-cmd "bash tool/stop_process_with_port.sh 8000" --a ${OMS_HOST} -p 8008
+npm run frontend:ci
 
-sleep 20
+sleep 10
